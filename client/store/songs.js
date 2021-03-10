3a5c21b40dc6cfdbcea523399522b37dbd504336
Kayla <frankumk@gmail.com>
2021-03-09 21:48:46 -0800

    Added menu to select genre

--- client/store/songs.js
+++ client/store/songs.js
@@ -13,13 +13,53 @@ const _loadSongs = (songs) => ({type: LOAD_SONGS, songs})
 /**
  * THUNK CREATORS
  */
-export const loadSongs = () => {
+
+
+export const loadSongs = (genre) => {
     return async (dispatch) => {
-        const response = await youtube.get("/playlistItems", {
+      let response;
+      if(genre==='Top50'){
+        response = await youtube.get("/playlistItems", {
         params: {
+          //2021
+          playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
+          //shower id:K1vKpAvLUr4
+          //chillstep: bLNzGxcY658
+          //hotgirls: PL2AHJxGoZJdq_gS_KNhXkVBk-MVp1rIPk
+        },
+        })
+      }else if(genre==='HotGirl'){
+        response = await youtube.get("/playlistItems", {
+          params: {
+            playlistId: "PL2AHJxGoZJdq_gS_KNhXkVBk-MVp1rIPk",
+          }
+        })
+      }else if(genre==='Reggae'){
+        response = await youtube.get("/playlistItems", {
+          params: {
+            playlistId: "RDGMEM29nh-so2GiiVvCzzeO3LJQ",
+          }
+        })
+      }else if(genre==='Rock'){
+        response = await youtube.get("/playlistItems", {
+          params: {
+            playlistId: "RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI",
+          }
+        })
+      }else if(genre==='Workout'){
+        response = await youtube.get("/playlistItems", {
+          params: {
+            playlistId: "PLChOO_ZAB22WAvnFw86vUueyv026ULwIv",
+          }
+        })
+      }else{
+      response = await youtube.get("/playlistItems", {
+        params: {
+          //2021
           playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
         },
-        });
+        })
+    }
         return dispatch(_loadSongs(response.data.items))
   }
 }
