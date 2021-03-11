96761ddc87e7dea8ce4c964f1e7faec656a3c665
Kayla <frankumk@gmail.com>
2021-03-10 19:23:40 -0800

    Switch

--- client/store/songs.js
+++ client/store/songs.js
@@ -64,45 +64,6 @@ export const loadSongs = (genre) => {
         })
         break
       }
-      // if(genre==='Top50'){
-      //   response = await youtube.get("/playlistItems", {
-      //   params: {
-      //     //2021
-      //     playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-      //   },
-      //   })
-      // }else if(genre==='Soundbath'){
-      //   response = await youtube.get("/playlistItems", {
-      //     params: {
-      //       playlistId: "PL_4s4N3ooC9T0Roc-lnB_nYESCSPzh-nB",
-      //     }
-      //   })
-      // }else if(genre==='Reggae'){
-      //   response = await youtube.get("/playlistItems", {
-      //     params: {
-      //       playlistId: "RDGMEM29nh-so2GiiVvCzzeO3LJQ",
-      //     }
-      //   })
-      // }else if(genre==='Rock'){
-      //   response = await youtube.get("/playlistItems", {
-      //     params: {
-      //       playlistId: "RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI",
-      //     }
-      //   })
-      // }else if(genre==='Workout'){
-      //   response = await youtube.get("/playlistItems", {
-      //     params: {
-      //       playlistId: "RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI",
-      //     }
-      //   })
-      // }else{
-      // response = await youtube.get("/playlistItems", {
-      //   params: {
-      //     //2021
-      //     playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-      //   },
-      //   })
-      // }
         return dispatch(_loadSongs(response.data.items))
   }
 }
