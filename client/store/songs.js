1aea71e2cb81408bea18ee2cfaf1bb45529fbf65
Kayla <frankumk@gmail.com>
2021-03-10 17:22:38 -0800

    switch didnt work

--- client/store/songs.js
+++ client/store/songs.js
@@ -18,48 +18,85 @@ const _loadSongs = (songs) => ({type: LOAD_SONGS, songs})
 export const loadSongs = (genre) => {
     return async (dispatch) => {
       let response;
-      if(genre==='Top50'){
-        response = await youtube.get("/playlistItems", {
-        params: {
-          //2021
-          playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-          //shower id:K1vKpAvLUr4
-          //chillstep: bLNzGxcY658
-          //hotgirls: PL2AHJxGoZJdq_gS_KNhXkVBk-MVp1rIPk
-        },
+      switch(genre){
+        case 'Top50':
+          response = await youtube.get("/playlistItems", {
+              params: {
+                //2021
+                playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
+              },
+          })
+        case 'Soundbath':
+          response = await youtube.get("/playlistItems", {
+                params: {
+                  playlistId: "PL_4s4N3ooC9T0Roc-lnB_nYESCSPzh-nB",
+                }
         })
-      }else if(genre==='Soundbath'){
-        response = await youtube.get("/playlistItems", {
-          params: {
-            playlistId: "PL_4s4N3ooC9T0Roc-lnB_nYESCSPzh-nB",
-          }
-        })
-      }else if(genre==='Reggae'){
-        response = await youtube.get("/playlistItems", {
+        case "Reggae":
+          response = await youtube.get("/playlistItems", {
           params: {
             playlistId: "RDGMEM29nh-so2GiiVvCzzeO3LJQ",
           }
         })
-      }else if(genre==='Rock'){
+        case "Rock":
         response = await youtube.get("/playlistItems", {
           params: {
             playlistId: "RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI",
           }
         })
-      }else if(genre==='Workout'){
-        response = await youtube.get("/playlistItems", {
-          params: {
-            playlistId: "PLChOO_ZAB22WAvnFw86vUueyv026ULwIv",
+        case "Workout":
+          response = await youtube.get("/playlistItems", {
+            params: {
+            playlistId: "RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI",
           }
         })
-      }else{
-      response = await youtube.get("/playlistItems", {
-        params: {
-          //2021
-          playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
-        },
+        default:
+          response = await youtube.get("/playlistItems", {
+            params: {
+              //2021
+              playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
+            },
         })
-    }
+      }
+      // if(genre==='Top50'){
+      //   response = await youtube.get("/playlistItems", {
+      //   params: {
+      //     //2021
+      //     playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
+      //   },
+      //   })
+      // }else if(genre==='Soundbath'){
+      //   response = await youtube.get("/playlistItems", {
+      //     params: {
+      //       playlistId: "PL_4s4N3ooC9T0Roc-lnB_nYESCSPzh-nB",
+      //     }
+      //   })
+      // }else if(genre==='Reggae'){
+      //   response = await youtube.get("/playlistItems", {
+      //     params: {
+      //       playlistId: "RDGMEM29nh-so2GiiVvCzzeO3LJQ",
+      //     }
+      //   })
+      // }else if(genre==='Rock'){
+      //   response = await youtube.get("/playlistItems", {
+      //     params: {
+      //       playlistId: "RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI",
+      //     }
+      //   })
+      // }else if(genre==='Workout'){
+      //   response = await youtube.get("/playlistItems", {
+      //     params: {
+      //       playlistId: "RDCLAK5uy_mfut9V_o1n9nVG_m5yZ3ztCif29AHUffI",
+      //     }
+      //   })
+      // }else{
+      // response = await youtube.get("/playlistItems", {
+      //   params: {
+      //     //2021
+      //     playlistId: "PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU",
+      //   },
+      //   })
+      // }
         return dispatch(_loadSongs(response.data.items))
   }
 }
