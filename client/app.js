b5e04c2217d312b69842f58de1e7e4082854a0ab
Kayla <frankumk@gmail.com>
2021-03-22 20:18:28 -0700

    added a theme for mui override

--- client/app.js
+++ client/app.js
@@ -3,12 +3,27 @@ import React from 'react'
 import Navbar from './components/navbar'
 import Routes from './routes'
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+       main: "#000080"
+              },
+    secondary: {
+       main: "#34ebe5"
+               }
+          }
+})
+
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    </ThemeProvider>
   )
 }
 
