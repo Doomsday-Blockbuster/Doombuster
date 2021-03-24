39eef76234a5a5ffbe355091a147f342023b14fa
Kayla <frankumk@gmail.com>
2021-03-23 19:44:25 -0700

    thumbs purple

--- client/components/styledIcon.js
+++ client/components/styledIcon.js
@@ -5,38 +5,30 @@ import {HandThumbsDown, HandThumbsUp} from '@styled-icons/bootstrap'
 
 
 export const ThumbUp = styled(HandThumbsUp)`
-  color: red;
+  color: purple;
   font-weight: bold;
-  margin: 0.5rem;
+  margin: 0.25rem;
   &:hover {
     box-shadow: inset 0 0 10px #000000;
     cursor: pointer;
   }
-  box-shadow: ${(props) => (props.voteType=== 'upvote' ? 'inset 0 0 10px #000000' : 'none') };
+  
 `
 
 export const ThumbDown = styled(HandThumbsDown)`
-  color: red;
+  color: purple;
   font-weight: bold;
-  margin: 0.5rem;
+  margin: 0.25rem;
   &:hover {
     box-shadow: inset 0 0 10px #000000;
     curser: pointer;
   }
-  box-shadow: ${(props) => (props.voteType === 'downvote' ? 'inset 0 0 10px #000000' : 'none') };
+  
   
 
 `
 
 // ${(props) => (props.important ? 'bold' : 'normal') }
 
-//export const IconStyleWrapper = 
-
-// import {StyledIconBase} from '@styled-icons/styled-icon'
-
-// export const IconStyleWrapper = styled.div`
-//   ${StyledIconBase} {
-//     color: red;
-//     /* icon styles go here */
-//   }
-// `
+//box-shadow: ${(props) => (props.voteType === 'downvote' ? 'inset 0 0 10px #000000' : 'none') };
+// box-shadow: ${(props) => (props.voteType=== 'upvote' ? 'inset 0 0 10px #000000' : 'none') };
