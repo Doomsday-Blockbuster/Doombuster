b8d98a817b1a8b7c6a15d577eae02c1ea61161b2
MonilGoklani <monil2912@gmail.com>
2021-03-26 17:56:23 -0400

    fixed non-admin view css

--- client/components/playqueue.js
+++ client/components/playqueue.js
@@ -1,28 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
 import VideoPlayer from "./videoplayer";
-import Card from '@material-ui/core/Card';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles'
+import Card from "@material-ui/core/Card";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
 const styles = () => ({
   root: {
-    backgroundColor:'#050e4a',
-    color:'white',
-    overflow: 'hidden',
-    width:'100%',
+    backgroundColor: "#050e4a",
+    color: "white",
+    overflow: "hidden",
+    width: "100%",
   },
-  topSong:{
-    backgroundColor:'#471061',
-    color:'white',
-    overflow: 'hidden',
-    width:'100%',
+  topSong: {
+    backgroundColor: "#471061",
+    color: "white",
+    overflow: "hidden",
+    width: "100%",
   },
-  songName:{
-    maxWidth:'375px',
-    padding: '0 0 0 1em',
-    fontFamily : 'Eight Bit',
-    opacity:'80%'
+  songName: {
+    maxWidth: "375px",
+    padding: "0 0 0 1em",
+    fontFamily: "Eight Bit",
+    opacity: "80%",
     // animation: 'floatText 12s infinite linear',
     // paddingLeft: '100%', /*Initial offset, which places the text off-screen*/
   },
@@ -32,8 +32,6 @@ class PlayQueue extends React.Component {
   constructor() {
     super();
   }
-
-  
 
   render() {
     const { queue, isAdmin, classes } = this.props;
@@ -50,45 +48,74 @@ class PlayQueue extends React.Component {
                 <img id="placeholder" src="../placeholder.jpg" />
               )}
             </div>
-            {topThree.length>0?(
-            <div id="topThree">
+            {topThree.length > 0 ? (
+              <div id="topThree">
                 <h1>top 3</h1>
                 {topThree.map((song) => {
                   return (
-                    <div id='topThreeItem'>
+                    <div id="topThreeItem">
                       <img src={song.image}></img>
-                      <Card key={song.id} className={song.rank===1?classes.topSong:classes.root}>
+                      <Card
+                        key={song.id}
+                        className={
+                          song.rank === 1 ? classes.topSong : classes.root
+                        }
+                      >
                         <p className={classes.songName}>{song.name}</p>
                       </Card>
                     </div>
                   );
                 })}
-            </div>):('')}
-          </div>
-        ) : (
-          <div>
-            {queue.length > 0 ? (
-              <div>
-                <div
-                  style={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column",
-                  }}
-                >
-                  <h3>{queue[0].name}</h3>
-                  <img id="largeThumbnail" src={queue[0].largeImage} />
-                  <h3>CURRENTLY PLAYING</h3>
-                </div>
-                <div id="topThree">
-                  <p>Hi</p>
-                </div>
               </div>
             ) : (
-              <div>
-                <img id="placeholder" src="../placeholder.jpg" />
+              ""
+            )}
+          </div>
+        ) : (
+          <div id="playerBarItems">
+            <div id="player">
+              {queue.length > 0 ? (
+                <div>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                      flexDirection: "column",
+                      margin: "0",
+                    }}
+                  >
+                    <p className='currentSongName'>{queue[0].name}</p>
+                    <img id="largeThumbnail" src={queue[0].largeImage} />
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <img id="placeholder" src="../placeholder.jpg" />
+                </div>
+              )}
+            </div>
+            {topThree.length > 0 ? (
+              <div id="topThree">
+                <h1>top 3</h1>
+                {topThree.map((song) => {
+                  return (
+                    <div id="topThreeItem">
+                      <img src={song.image}></img>
+                      <Card
+                        key={song.id}
+                        className={
+                          song.rank === 1 ? classes.topSong : classes.root
+                        }
+                      >
+                        <p className={classes.songName}>{song.name}</p>
+                      </Card>
+                    </div>
+                  );
+                })}
               </div>
+            ) : (
+              ""
             )}
           </div>
         )}
